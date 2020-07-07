442e450ff6e059deab1678ce787377cda84fc937
nanxiuzi <huangrunxingxing@163.com>
2020-07-07 11:53:07 +0800

    asdas

---
+++ src/main/java/cn/richinfo/kafka/TestIntervalJoin.java
@@ -0,0 +1,20 @@
+package cn.richinfo.kafka;
+
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+/**
+ * 功能描述:
+ *
+ * @ClassName: TestIntervalJoin
+ * @Author: huangrx丶
+ * @Date: 2020/7/7 11:48
+ * @Version: V1.0
+ */
+public class TestIntervalJoin {
+    public static void main(String[] args) {
+        StreamExecutionEnvironment ev = StreamExecutionEnvironment.getExecutionEnvironment();
+        ev
+    }
+
+
+}
