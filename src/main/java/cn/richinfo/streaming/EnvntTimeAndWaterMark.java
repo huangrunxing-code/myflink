859af0d82417d12a30732f33769a5a6cb58c6b12
huangrunxing <huangrunxingxing@163.com>
2020-09-14 17:33:11 +0800

    时间时间 水印复习

--- src/main/java/cn/richinfo/streaming/EnvntTimeAndWaterMark.java
+++ src/main/java/cn/richinfo/streaming/EnvntTimeAndWaterMark.java
@@ -20,13 +20,14 @@ public class EnvntTimeAndWaterMark {
         env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime);
         //设置水印生成时间间隔100ms
         env.getConfig().setAutoWatermarkInterval(100);
-        DataStreamSource<String> socds = env.socketTextStream("192.168.42.72", 9999);
+        DataStreamSource<String> socds = env.socketTextStream("192.168.42.71", 9009);
+        //分配时间戳
         DataStream<String> ds = socds.assignTimestampsAndWatermarks(new AssignerWithPeriodicWatermarks<String>() {
             @Nullable
             //当前水印时间戳
             private Long currentTimeStamp = 0L;
             //最大的允许乱序时间  5s
-            private Long maxOutofOrderness = 5000L;
+            private Long maxOutofOrderness = 2000L;
 
             @Override
             public Watermark getCurrentWatermark() {
@@ -36,35 +37,43 @@ public class EnvntTimeAndWaterMark {
             @Override
             //从数据里面提取时间戳
             public long extractTimestamp(String element, long previousElementTimestamp) {
-                Long mytime=Long.parseLong(element.split("\\|")[1]);
+                Long mytime=Long.parseLong(element.split(",")[1]);
                 currentTimeStamp=Math.max(mytime, currentTimeStamp);
                 //数据样例   name|timestamp
-                /*flink|1588659181000
-                flink|1588659182000
-                flink|1588659183000
-                flink|1588659184000
-                flink|1588659185000
-                flink|1588659180000
-                flink|1588659186000
-                flink|1588659187000
-                flink|1588659188000
-                flink|1588659189000*/
+                /*  flink,1588659181000
+                    flink,1588659182000
+                    flink,1588659183000
+                    flink,1588659184000
+                    flink,1588659185000
+                    flink,1588659186000
+
+                    flink,1588659181000
+                    flink,1588659182000
+                    flink,1588659183000
+                    flink,1588659184000
+                    flink,1588659185000
+                    flink,1588659180000
+                    flink,1588659186000
+                    flink,1588659187000
+                    flink,1588659188000
+                    flink,1588659189000
+                    flink,1588659190000
+                    */
                 System.err.println("===============mytime"+mytime+"===========watermark:"+(currentTimeStamp - maxOutofOrderness));
 
                 return mytime;
             }
         });
         ds.map(new MapFunction<String, Tuple2<String,Long>>() {
-
             @Override
             public Tuple2<String,Long> map(String s) throws Exception {
-                String[] split = s.split("\\|");
+                String[] split = s.split(",");
                 return new Tuple2<String,Long>(split[0],Long.parseLong(split[1])) ;
             }
         }).keyBy(0)
-          .timeWindow(Time.seconds(5))
+          .window(TumblingEventTimeWindows.of(Time.seconds(3)))
            .minBy(1)
-           .print();
+           .printToErr();
         env.execute("watermark");
     }
 }
